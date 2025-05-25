44b06da765d5680b4585bbd0a8380b1eb9fcdd74
Sachin Shrestha <sachin.shrestha42@gmail.com>
2025-05-26 06:52:13 +1000

    Change

---
+++ research/Scripts/schwab-order-codegen.py
@@ -0,0 +1,6 @@
+#!C:\Users\sachi\Documents\Researchcode\research\python.exe
+from schwab.scripts.orders_codegen import latest_order_main
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(latest_order_main(sys.argv[1:]))
